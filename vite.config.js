bcec81c23db7aabf4e68522a05dd7d4e65387f9a
foilem0 <foilem@proton.me>
2026-04-12 00:05:23 +0300

    got vite now

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: './',
+  publicDir: 'public',
+  build: {
+    target: 'esnext',
+    outDir: 'dist',
+    minify: 'esbuild',
+    cssCodeSplit: true,
+    sourcemap: false,
+    rollupOptions: {
+      input: './index.html',
+      output: {
+        dir: 'dist'
+      }
+    }
+  },
+  server: {
+    port: 5173,
+    open: true,
+    strictPort: false
+  }
+});
